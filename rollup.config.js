aede1165653560e0ba9f4bf5fadf60e3d5350c0e
alan <alan@fpi.cc>
2022-07-05 18:20:40 +0800

    test: add one test

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ const name = require("./package.json").main.replace(/\.js$/, "");
 const bundle = (config) => ({
   ...config,
   input: "src/index.ts",
+  // todo: resolve problem about externals
 });
 
 export default [
